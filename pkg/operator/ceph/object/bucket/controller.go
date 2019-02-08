65393b25dc05e59ff6896c15f080baebf08f0009
Scott Creeley <screeley@redhat.com>
2019-02-08 15:07:32 -0500

    merge of obc-controller code

--- pkg/operator/ceph/object/bucket/controller.go
+++ pkg/operator/ceph/object/bucket/controller.go
@@ -20,15 +20,16 @@ import (
 	"fmt"
 	"reflect"
 
-	// "github.com/aws/aws-sdk-go/aws"
-	// "github.com/aws/aws-sdk-go/aws/credentials"
-	// "github.com/aws/aws-sdk-go/aws/session"
-	// "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 
 	"github.com/coreos/pkg/capnslog"
 	opkit "github.com/rook/operator-kit"
 	"k8s.io/api/core/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apiserver/pkg/storage/names"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
@@ -37,6 +38,7 @@ import (
 
 	"k8s.io/client-go/informers"
 
+	// rook "github.com/rook/rook/pkg/apis/rook.io/v1alpha2" <-- unused in this branch
 	cephv1beta1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1beta1"
 	"github.com/rook/rook/pkg/client/informers/externalversions"
 	obListerv1beta1 "github.com/rook/rook/pkg/client/listers/ceph.rook.io/v1beta1"
@@ -49,6 +51,10 @@ const (
 	secretKey       string = "SecretKey"
 	crdNameSingular string = "cephobjectbucket"
 	crdNamePlural   string = "cephobjectbuckets"
+	//accessKeyId     string = "accessKeyId"
+	//secretAccessKey string = "secretAccessKey"
+	//crdNameSingular string = "objectbucketclaim"
+	//crdNamePlural   string = "objectbucketclaims"
 )
 
 // ObjectBucketResource represent the object store user custom resource for the watcher
@@ -99,13 +105,24 @@ func (c *Controller) StartWatch(stopCh <-chan struct{}) {
 	return
 }
 
+// Replaced by obc-controller branch - keeping for reference
+//func (c *Controller) onAdd(obj interface{}) {
+//	objectBucket, err := getObjectBucketResource(obj)
+//	if err != nil {
+//		logger.Errorf("failed to get CephObjectBucket resource: %v", err)
+//		return
+//	}
+//	c.handleAdd(objectBucket)
+//}
+
+// From obc-controller branch
 func (c *Controller) onAdd(obj interface{}) {
-	objectBucket, err := getObjectBucketResource(obj)
+	obc, err := copyObjectBucketClaim(obj)
 	if err != nil {
-		logger.Errorf("failed to get CephObjectBucket resource: %v", err)
+		logger.Errorf("failed to get ObjectBucketClaim resource: %v", err)
 		return
 	}
-	c.handleAdd(objectBucket)
+	c.handleAdd(obc)
 }
 
 func (c *Controller) onUpdate(oldObj, newObj interface{}) {
@@ -120,21 +137,39 @@ type cephUser struct {
 	name, accessKey, secretKey string
 }
 
-func (c *Controller) handleAdd(ob *cephv1beta1.CephObjectBucket) {
-	var err error
-	// get user and credentials
-	user, err := c.newCephUserFromObjectBucket(ob)
+// From obc-controller branch
+func (c *Controller) handleAdd(obc *cephv1beta1.CephObjectBucket) {
+	accessKey, secretKey, err := c.getAccessKeys(obc)
 	if err != nil {
-		logger.Errorf("failed to retrieve ceph user: %v", err)
+		logger.Errorf("failed to extract access keys: %v", err)
 		return
 	}
-	var _ = user
 
-	// s3-sdk create bucket with user/creds
-	// createCephBucket(user, ob.Name)
+	bucketName := names.SimpleNameGenerator.GenerateName(obc.Spec.GenerateBucketName)
 
+	err = createBucket(bucketName, accessKey, secretKey)
+	if err != nil {
+		logger.Errorf("could not create bucket: %v", err)
+	}
 	// create configMap
 }
+
+// Replaced by obc-controller branch - keeping for reference
+//func (c *Controller) handleAdd(ob *cephv1beta1.CephObjectBucket) {
+//	var err error
+//	// get user and credentials
+//	user, err := c.newCephUserFromObjectBucket(ob)
+//	if err != nil {
+//		logger.Errorf("failed to retrieve ceph user: %v", err)
+//		return
+//	}
+//	var _ = user
+//
+//	// s3-sdk create bucket with user/creds
+//	// createCephBucket(user, ob.Name)
+//
+//	// create configMap
+//}
 
 func (c *Controller) handleUpdate(ob *cephv1beta1.CephObjectBucket) {
 	// TODO
@@ -144,6 +179,16 @@ func (c *Controller) handleDelete(ob *cephv1beta1.CephObjectBucket) {
 	// TODO
 }
 
+func copyObjectBucketClaim(obj interface{}) (*cephv1beta1.CephObjectBucket, error) {
+	var ok bool
+	obc, ok := obj.(*cephv1beta1.CephObjectBucket)
+	if !ok {
+		return nil, fmt.Errorf("type check failed, not an ObjectBucketClaim: %+v", obj)
+	}
+	return obc.DeepCopy(), nil
+}
+
+//TODO maybe can be removed since it's not used - but keeping as reference for now?
 func getObjectBucketResource(obj interface{}) (*cephv1beta1.CephObjectBucket, error) {
 	var ok bool
 	objectBucket, ok := obj.(*cephv1beta1.CephObjectBucket)
@@ -204,6 +249,66 @@ func (c *Controller) getObjectBucketUserSecret(user, namespace string) (*v1.Secr
 	}
 	return secretList[0], nil
 }
+
+func (c *Controller) getAccessKeys(obc *cephv1beta1.CephObjectBucket) (string, string, error) {
+	//TODO - obc.Spec.BucketName isn't right - need to add status.secretName field or something to CephObjectBucket
+	secret, err := c.getAccessKeySecret(obc.Spec.BucketName, obc.Namespace)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to get user credentials")
+	}
+	ak := string(secret.Data[accessKey])
+	sk := string(secret.Data[secretKey])
+	if len(ak) == 0 || len(sk) == 0 {
+		return "", "", fmt.Errorf("invalid secret key value, key(s) cannot be empty")
+	}
+	return ak, sk, nil
+}
+
+func (c *Controller) getAccessKeySecret(secretName, namespace string) (*v1.Secret, error) {
+	sec, err := c.secretLister.Secrets(namespace).Get(secretName)
+	if err != nil {
+		return nil, fmt.Errorf("could not list user secrets by 'user' label: %v", err)
+	}
+	return sec, nil
+}
+
+func createBucket(bucketName, accessKey, secretKey string) error {
+	// Instantiate a short lived S3 client
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
+	}))
+	s3c := s3.New(sess)
+
+	output, err := s3c.CreateBucket(&s3.CreateBucketInput{
+		Bucket: &bucketName,
+	})
+	logger.Infof("bucket creation output: %v", output)
+	if err != nil {
+		return fmt.Errorf("failed creating bucket %q: %v", bucketName, err)
+	}
+	return nil
+}
+
+// From obc-controller branch
+//
+// // TODO
+// func newBucketConfigMap() *v1.ConfigMap {
+// 	return &v1.ConfigMap{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name: rookPrefix + ob.Name,
+// 		},
+// 		Data: map[string]string{
+// 			"BUCKET_HOST": "",
+// 			"BUCKET_PORT": "",
+// 			"BUCKET_NAME": "",
+// 			"BUCKET_SSL":  "",
+// 		},
+// 	}
+// }
+
+// From original object-bucket-controller branch
+//TODO
+// These commented out prior to branch merge - keeping as reference for now
 
 // func createCephBucket(user *cephUser, bucketName string) error {
 // 	s3Client := newS3ClientFromCreds(user.accessKey, user.secretKey)
